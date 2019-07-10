c1f32ecb7de71c94279f834048ccf63ac24b750a
AGMETEOR <guwats10@gmail.com>
2019-07-10 19:49:23 +0300

    Initial project module set up

---
+++ collections/collections_test.go
@@ -0,0 +1,10 @@
+package collections
+
+import "testing"
+
+func TestCollections(t *testing.T) {
+    want := "Hello, world."
+    if got := Hello(); got != want {
+        t.Errorf("Hello() = %q, want %q", got, want)
+    }
+}
