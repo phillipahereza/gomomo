20abdd5ba36bb5d68b6c3deccb86f045f23c0a1f
phillipahereza <pahereza@gmail.com>
2020-04-03 12:59:47 +0300

    added make file to build executable

--- go.mod
+++ go.mod
@@ -1,9 +1,5 @@
-module github.com/phillipahereza/mtnmomo
+module github.com/phillipahereza/momoapi-go
 
 go 1.12
 
-require (
-	golang.org/x/text v0.3.2 // indirect
-	rsc.io/quote/v3 v3.1.0
-	rsc.io/sampler v1.3.1 // indirect
-)
+require github.com/urfave/cli/v2 v2.2.0
--- main.go
+++ main.go
@@ -2,9 +2,46 @@ package main
 
 import (
 	"fmt"
-	"github.com/phillipahereza/mtnmomo/collections"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println(collections.Collections())
+	app := cli.NewApp()
+
+	app.Commands = []*cli.Command {
+		{
+			Name:    "sandbox-user",
+			Aliases: []string{"sandbox"},
+			Usage:   "Create a sandbox user",
+			Action:  createSandboxUserCmd,
+			Flags: []cli.Flag {
+				&cli.StringFlag{
+					Name: "callback",
+					Aliases: []string{"c"},
+					Value: "",
+					Usage: "Your callback host .e.g. http://myapp.com",
+					Required: true,
+				},
+				&cli.StringFlag{
+					Name: "key",
+					Aliases: []string{"k"},
+					Value: "",
+					Usage: "Subscription key which provides access to this API. Found in your profile Primary Key",
+					Required: true,
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createSandboxUserCmd(c *cli.Context) error {
+	fmt.Printf("%s - %s\n", c.String("callback"), c.String("key"))
+	return nil
 }
